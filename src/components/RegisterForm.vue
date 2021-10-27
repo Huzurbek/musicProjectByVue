ba2a69547ce89cd4382955ade95179ad086eed44
Huzurbek <huzurbek@yahoo.com>
2021-10-27 11:17:35 +0500

    Setting up Login form with firebase

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -81,8 +81,6 @@
 </template>
 
 <script>
-import firebase from '@/includes/firebase';
-
 export default {
   name: 'RegisterForm',
   data() {
@@ -116,11 +114,8 @@ export default {
       this.reg_alert_variant = 'bg-blue-500';
       this.reg_alert_msg = 'Please wait! Your account is being created.';
 
-      let userCred = null;
       try {
-        userCred = await firebase.auth().createUserWithEmailAndPassword(
-          values.email, values.password,
-        );
+        await this.$store.dispatch('register', values);
       } catch (error) {
         this.reg_in_submission = false;
         this.reg_alert_variant = 'bg-red-500';
@@ -130,7 +125,6 @@ export default {
 
       this.reg_alert_variant = 'bg-green-500';
       this.reg_alert_msg = 'Success! Your account has been created.';
-      console.log(userCred);
     },
   },
 };
--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyC3Ecl7ee0vMEoHHXigeV-EFMMUVuugToM',
@@ -10,4 +11,15 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export default firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+const usersCollection = db.collection('users');
+
+export {
+  auth,
+  db,
+  usersCollection,
+};
