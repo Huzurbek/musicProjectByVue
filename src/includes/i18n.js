e134bf7a2b80c313511f0bf4003faf254836ca5c
Huzurbek <huzurbek@yahoo.com>
2021-11-06 09:18:18 +0500

    Our first translation

--- src/includes/i18n.js
+++ src/includes/i18n.js
@@ -1,13 +1,15 @@
+/* eslint-disable max-len */
 import { createI18n } from 'vue-i18n';
 
 /**
  * Load locale messages
  *
+ // eslint-disable-next-line max-len
  * The loaded `JSON` locale messages is pre-compiled by `@intlify/vue-i18n-loader`, which is integrated into `vue-cli-plugin-i18n`.
  * See: https://github.com/intlify/vue-i18n-loader#rocket-i18n-resource-pre-compilation
  */
 function loadLocaleMessages() {
-  const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i);
+  const locales = require.context('../locales', true, /[A-Za-z0-9-_,\s]+\.json$/i);
   const messages = {};
   locales.keys().forEach((key) => {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i);
@@ -18,7 +20,7 @@ function loadLocaleMessages() {
   });
   return messages;
 }
-
+// locale: process.env.VUE_APP_I18N_LOCALE || 'en',
 export default createI18n({
   locale: process.env.VUE_APP_I18N_LOCALE || 'en',
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
