a6644a4c382f68307fa9d7417096539f086c26ac
Huzurbek <huzurbek@yahoo.com>
2021-11-06 08:46:23 +0500

    make Internationalization

--- src/directives/icon-secondary.js
+++ src/directives/icon-secondary.js
@@ -1,10 +1,15 @@
 export default {
   beforeMount(el, binding) {
     // eslint-disable-next-line no-param-reassign
-    let iconClass = `fa fa-${binding.value.icon} text-green-40 text-2xl`;
+    let iconClass = `fa fa-${binding.value.icon} text-2xl`;
 
     if (binding.value.right) {
       iconClass += ' float-right';
+    }
+    if (binding.value.green) {
+      iconClass += ' text-green-400';
+    } else {
+      iconClass += ' text-red-400';
     }
 
     el.innerHTML += `<i class="${iconClass}"></i>`;
