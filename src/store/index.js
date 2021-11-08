0f0123baccc41eac8335ef18fa51db20e8e7df7f
Huzurbek <huzurbek@yahoo.com>
2021-11-08 11:56:13 +0500

    registering modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,7 @@
 import { createStore } from 'vuex';
-import auth from './modules/auth';
-import player from './modules/player';
+import modules from './modules';
 
 export default createStore({
-  modules: {
-    auth,
-    player,
-  },
+  modules,
 
 });
